0129642ffcb8107ebeacf5999b2af40f0b52f54f
Fred Bronze <fredbronze@gmail.com>
2019-04-28 14:53:15 +0100

    Split off Encore Knuckles sprites after all

--- General/Sprites/Knuckles/Cutscene/DPLC - MHZ Press.asm
+++ General/Sprites/Knuckles/Cutscene/DPLC - MHZ Press.asm
@@ -3,17 +3,17 @@ DPLC_66B10:		dc.w word_66B18-DPLC_66B10
 		dc.w word_66B24-DPLC_66B10
 		dc.w word_66B2A-DPLC_66B10
 word_66B18:	dc.w 2			; DATA XREF: ROM:00066B10o
-		dc.w $18C5
-		dc.w $2717
-		dc.w $1AC3
+		dc.w $5785
+		dc.w $837
+		dc.w $E3
 word_66B1E:	dc.w 2		; DATA XREF: ROM:00066B10o1AC3
-		dc.w $2792
-		dc.w $27CB
-		dc.w $1AC3
+		dc.w $8B2
+		dc.w $8EB
+		dc.w $E3
 word_66B24:	dc.w 2			; DATA XREF: ROM:00066B10o27CB
-		dc.w $2881
-		dc.w $28AB
-		dc.w $1AC3
+		dc.w $9A1
+		dc.w $9CB
+		dc.w $E3
 word_66B2A:	dc.w 1			; DATA XREF: ROM:00066B10o2881
-		dc.w $18C5
-		dc.w $192B
+		dc.w $5785
+		dc.w $57EB
--- General/Sprites/Knuckles/Cutscene/Map - MHZ2 Press.asm
+++ General/Sprites/Knuckles/Cutscene/Map - MHZ2 Press.asm
@@ -7,11 +7,11 @@ word_66AD8:	dc.w 3
 		dc.b  $FC,  $D,	  8,   6, $FF, $F0
 		dc.b   $C,  $C,	  8,  $E, $FF, $F0
 word_66AE6:	dc.w 3			
-		dc.b  $EC,   8,	  0,   0, $FF, $F8
+		dc.b  $EC,   8,	  8,   0, $FF, $F8
 		dc.b  $F4,  $E,	  8,   3, $FF, $F0
 		dc.b   $C,  $C,	  8,  $F, $FF, $F0
 word_66AF4:	dc.w 3			
-		dc.b  $EC,   4,	  0,   0, $FF, $F8
+		dc.b  $EC,   4,	  8,   0, $FF, $F8
 		dc.b  $F4,  $E,	  8,   2, $FF, $F0
 		dc.b   $C,  $C,	  8,  $E, $FF, $F0
 word_66B02:	dc.w 2			
